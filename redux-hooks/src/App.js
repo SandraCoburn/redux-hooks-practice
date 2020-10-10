889629876db3d47f02dbe48be2ebe54130d051cf
Sandra Coburn <sandra_coburn@hotmail.com>
2020-10-10 13:24:13 -0700

    Added Redux store and all components

--- redux-hooks/src/App.js
+++ redux-hooks/src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import Navigation from "./components/Nav/Navigation";
+import ProductsPage from "./containers/Products";
+import FavoritesPage from "./containers/Favorites";
 
 const App = (props) => {
   return (
     <React.Fragment>
       <Navigation />
       <main>
-        <Route path="/" />
-        <Route path="/favorites" />
+        <Route path="/" component={ProductsPage} exact />
+        <Route path="/favorites" component={FavoritesPage} />
       </main>
     </React.Fragment>
   );
